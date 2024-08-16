fc4f24c75bf27cd240da9e4f9cfe5dcb4e9f66f7
asifrahman <164550776+Asifrahman85@users.noreply.github.com>
2024-08-16 21:04:07 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,16 +23,15 @@ const Header = () => {
   };
 
   return (
-    <div className="h-screen border-8 relative overflow-hidden"> {/* Added overflow-hidden */}
+    <div className="h-screen  relative overflow-hidden">
       <Navbar />
-
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
         <img
           src={bg1}
           alt="bg1"
           className="pt-[10%] w-[35%] h-[89%] absolute right-0 top-0"
-        /> {/* Corrected absolute positioning */}
-        <div className="relative h-screen flex flex-col items-start justify-center gap-8 w-4/6 pl-[7%] mt-8">
+        />
+        <div className="relative h-screen flex flex-col items-start justify-center gap-8 md:w-4/6 pl-[7%] mt-8">
           <div className="absolute top-0 right-0 p-1 mx-6 flex h-36 justify-end bg-[#FF6347] text-[#FF6347]">
             hi
           </div>
@@ -73,7 +72,7 @@ const Header = () => {
         </div>
 
         <div className="flex items-start mt-8 flex-col h-2/6">
-          <Slider {...settings} className="relative w-[465px] h-[44rem]">
+          <Slider {...settings} className="relative w-full md:w-[465px] h-[44rem]">
             <div>
               <img src={slide1} alt="Slide 1" className="w-full h-full object-cover" />
             </div>
