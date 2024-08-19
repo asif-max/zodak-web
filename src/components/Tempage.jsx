7a5d0f82c42686a1db3ee34eb4f409b1d2fcd9af
asifrahman <164550776+Asifrahman85@users.noreply.github.com>
2024-08-19 08:19:25 +0530

    Update Tempage.jsx

--- src/components/Tempage.jsx
+++ src/components/Tempage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import rulingImg from "../assets/zodak/images/Asset 3.png";
 import spyralimage from "../assets/zodak/images/spyralimage.png";
-import { motion } from "framer-motion";
+import { motion,delay } from "framer-motion";
+
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      delay: delay,
+    },
+  },
+});
+
 
 const Tempage = () => {
   return (
@@ -14,7 +28,7 @@ const Tempage = () => {
         </h1>
       </div>
       <div className="w-1/2 relative flex justify-center items-end">
-        <motion.div  className="absolute top-[23%] left-1/2 transform -translate-x-1/2 -translate-y-[120%] z-10 text-center font-semibold text-2xl text-black">
+        <motion.div   className="absolute top-[23%] left-1/2 transform -translate-x-1/2 -translate-y-[120%] z-10 text-center font-semibold text-2xl text-black">
          <span className="text-primary">SAFETY </span>  SOLUTIONS <br /> ACROSS INDUSTRIES
         </motion.div>
         <img
@@ -25,34 +39,34 @@ const Tempage = () => {
         <div className="w-[55%] mr-10 z-10 space-y-12 bottom-10 mb-24">
           <div className="flex justify-end">
             <div className="w-1/2 flex flex-col items-start eachone">
-              <p className="font-bold text-primary text-sm">sample heading</p>
-              <p className="text-xs text-start tracking-tight">
+              <motion.p whileInView={{ opacity: 1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}} className="font-bold text-primary text-sm">sample heading</motion.p>
+              <motion.p whileInView={{ opacity: 1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}} className="text-xs text-start tracking-tight">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, minus!
-              </p>
+              </motion.p>
             </div>
           </div>
           <div className="flex justify-start">
             <div className="w-1/2 flex flex-col items-start eachone">
-              <p className="font-bold text-primary text-sm">sample heading</p>
-              <p className="text-xs text-start tracking-tight">
+              <motion.p whileInView={{ opacity: 1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}} className="font-bold text-primary text-sm">sample heading</motion.p>
+              <motion.p whileInView={{ opacity: 1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}} className="text-xs text-start tracking-tight">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, minus!
-              </p>
+              </motion.p>
             </div>
           </div>
           <div className="flex justify-end">
             <div className="w-1/2 flex flex-col items-start eachone">
-              <p className="font-bold text-primary text-sm">sample heading</p>
-              <p className="text-xs text-start tracking-tight">
+              <motion.p whileInView={{ opacity: 1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}} className="font-bold text-primary text-sm">sample heading</motion.p>
+              <motion.p whileInView={{ opacity: 1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}} className="text-xs text-start tracking-tight">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, minus!
-              </p>
+              </motion.p>
             </div>
           </div>
           <div className="flex justify-start">
             <div className="w-1/2 flex flex-col items-start eachone">
-              <p className="font-bold text-primary text-sm">sample heading</p>
-              <p className="text-xs text-start tracking-tight">
+              <motion.p whileInView={{ opacity: 1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}} className="font-bold text-primary text-sm">sample heading</motion.p>
+              <motion.p whileInView={{ opacity: 1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}} className="text-xs text-start tracking-tight">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, minus!
-              </p>
+              </motion.p>
             </div>
           </div>
         </div>
