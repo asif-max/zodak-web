e39c812a9afcb6136aaf9ce0c88b98d8531940ee
asifrahman <164550776+Asifrahman85@users.noreply.github.com>
2024-08-21 11:25:37 +0530

    Update Tempage.jsx

--- src/components/Tempage.jsx
+++ src/components/Tempage.jsx
@@ -12,10 +12,10 @@ const Tempage = () => {
     <div className="flex flex-row bg-white">
       <div className="relative h-screen w-10/12">
         <img src={rulingImg} alt="" className="h-full object-cover brightness-50" />
-        <h1 className="absolute top-1/2 left-[29%] text-4xl text-end font-bold text-white">
+        <motion.h1 whileInView={{ opacity: 1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}} className="absolute top-1/2 left-[29%] text-4xl text-end font-bold text-white">
           ZODAK SAFETY <br /> GLOVES GIVE <br />
           EXCEPTIONAL PROTECTION
-        </h1>
+        </motion.h1>
       </div>
       <div className="w-1/2 relative flex justify-center items-end">
         <motion.div   className="absolute top-[23%] left-1/2 transform -translate-x-1/2 -translate-y-[120%] z-10 text-center font-semibold text-2xl text-black">
